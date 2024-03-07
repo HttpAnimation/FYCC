c63f6cfdf594e875edb94b4e659c805ec4788b5a
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-07 12:55:39 -0500

    new name

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 # compile.sh
 echo "Compileing"
-gcc `pkg-config --cflags gtk+-3.0` -o json_reader json_reader.c `pkg-config --libs gtk+-3.0`
+gcc `pkg-config --cflags gtk+-3.0` -o TestReadJson json_reader.c `pkg-config --libs gtk+-3.0`
 echo "Done"
