a9483ab9fb068c5880c9d477f766222cb73356cc
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-07 12:58:41 -0500

    compile

--- compile.sh
+++ compile.sh
@@ -3,3 +3,5 @@ echo "Compileing"
 echo "Script | READJSON.c"
 gcc `pkg-config --cflags gtk+-3.0` -o TestReadJson ReadJSON.c `pkg-config --libs gtk+-3.0`
 echo "Done"
+echo "Script | READConf.c"
+gcc `pkg-config --cflags gtk+-3.0` -o TestReadJson ReadJSON.c `pkg-config --libs gtk+-3.0`
